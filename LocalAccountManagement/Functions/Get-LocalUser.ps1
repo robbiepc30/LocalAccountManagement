0fce71d9055cc2a452e61b160440a8e5f704ccb1
Robert Courtney <robbiepc30@hotmail.com>
2016-07-22 11:46:57 -0800

    Get-LocalUser is working well, now writing more tests

--- LocalAccountManagement/Functions/Get-LocalUser.ps1
+++ LocalAccountManagement/Functions/Get-LocalUser.ps1
@@ -24,36 +24,43 @@ function Get-LocalUser{
     )
     Begin 
     {
-        
+        # KEEP THIS IN THE BEGIN BLOCK!!!PREVENTS DUPLICATE ACCOUNTS FROM DISPLAYING WHEN USING THE PIPELINE AND USING WILDCARDS
+        # pulls local accounts form all computers and stores them in array this is done
+        # in the Begin part so it prevents problems that can occure if a user uses wildcards that can cause duplicate accounts to show
+        # EX: The following accounts exist "Test1", "Test2", "TestGuest"
+        # "t*", "*guest*" | Get-LocalUser    ...  If this wasnt in the begin bock but was in the process block the it would display accounts twice
+        $computerAccounts = New-Object System.Collections.ArrayList
+        foreach ($C in $ComputerName) 
+        { # Do this for each computer in the $ComputerName collection          
+            [System.Collections.ArrayList]$localUsers = getAllUsers -ComputerName $C
+            [void]$computerAccounts.Add(@{'ComputerName' = $C
+                                    'UserAccounts' = $localUsers})
+        }
     }
     Process 
     { 
-        foreach ($C in $ComputerName) 
-        { # Do this for each computer in the $ComputerName collection
-            Write-Verbose "Retrieving users on `"$C`"" 
-            
-            # needed to be able to remove item from array
-            [System.Collections.ArrayList]$localUsers = getAllUsers -ComputerName $C
-
+        foreach ($ca in $computerAccounts) {
             foreach ($n in $Name) 
             {
-                $users = $localUsers | Where-Object { $_.name -like $n }
+                $users = $ca.UserAccounts | Where-Object { $_.name -like $n }
                 
                 # if a wildcard is used there may be more than one result, need to go through each one
                 foreach ($u in $users) 
                 {
                     $Obj = New-Object -TypeName PSObject -Property @{'Name' = $u.name.ToString()
-                                                                    'Description' = $u.description.ToString()}                    
-                    # remove record from array list, DO NOT REMOVE THIS
-                    # If comptuer has multipe accounts that start with a "t", one called "testguest" and and a acount called something "guest"
-                    # and uses wildcards to select user accounts such as: Get-LocalUser -Name t*, *guest* this would produce duplicate accounts.  By removing the item
-                    # from the array list this removes that possibility
-                    $localUsers.Remove($u) 
+                                                                    'Description' = $u.description.ToString()
+                                                                    'ComputerName' = $ca.ComputerName }                    
+                    # DO NOT REMOVE THIS!!! PREVENTS DUPLICATE ACCOUNTS FROM DISPLAYING WHEN USING -NAME PARAMATER AND USING WILDCARDS
+                    # remove record from array list 
+                    # If comptuer has multipe accounts that start with a "t", one called "testguest" and a user wanted to find all acounts that start with "t" and all
+                    # that have the name "guest" and used the following: Get-LocalUser -Name t*, *guest* this would produce duplicate accounts.
+                    # By removing the item from the array list this removes that possibility
+                    $ca.UserAccounts.Remove($u) 
                     
                     $Obj
                 }
-            }                  
-        }
+            }
+        }     
     }
 }
 
