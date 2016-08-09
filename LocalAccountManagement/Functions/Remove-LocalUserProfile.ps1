2c85bc25988fefd6f657f811b898f8f718cce0ed
Robert Courtney <robbiepc30@hotmail.com>
2016-08-08 16:14:10 -0800

    add validation to remove error for Remove-LocalUserProfile
    checks if user profile exist first before calling method Delete()

--- LocalAccountManagement/Functions/Remove-LocalUserProfile.ps1
+++ LocalAccountManagement/Functions/Remove-LocalUserProfile.ps1
@@ -1,3 +1,22 @@
+<#
+.Synopsis
+   Removes local users PROFILE from computer(s) 
+.DESCRIPTION
+   Removes local users PROFILE from computer(s) 
+.EXAMPLE
+   'User1', 'User2' | Remove-LocalUser  
+   Removes User1 and User2 from local computer
+.EXAMPLE
+   Remove-LocalUserProfile -Name 'User1', 'User2' -ComputerName 'Client01', Client02' -Force  
+   Removes User1 and User2 PROFILE from computer Client01 and Client02
+
+.EXAMPLE
+   Get-LocalUser *test* | Remove-LocalUserProfile   
+   Removes all users PROFILE from local computer with the name "test" anywhere in the username
+.EXAMPLE
+   Import-Csv UserAccounts.csv | Remove-LocalUser   
+   Removes all users PROFILE listed in the CSV on the Computers listed in the CSV file
+#>
 Function Remove-LocalUserProfile {
     [CmdletBinding(SupportsShouldProcess=$true,
                     confirmImpact='High')]
@@ -31,5 +50,6 @@ Function Remove-LocalUserProfile {
 
 function removeUserProfile ($Name, $ComputerName) {
     $UserProfile = Get-WmiObject Win32_UserProfile -ComputerName $ComputerName -filter  "LocalPath Like '$env:SystemDrive\\users\\$Name'" 
-    $UserProfile.Delete() #Deletes user profile
+    # if user proifle exists, call Delete mothod to remove profile
+    if ($UserProfile) { $UserProfile.Delete() }     
 }
